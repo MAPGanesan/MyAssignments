9f748bcdef7cc7df5c2935b0c8ba5950a25985b9
MAPGanesan <messagetoganesan@gmail.com>
2026-09-16 13:02:58 +0530

    Added steps in files

--- Week2/createLead.spec.ts
+++ Week2/createLead.spec.ts
@@ -1,3 +1,14 @@
+/* Create Lead
+1. Login to https://login.salesforce.com
+2. Click on toggle menu button from the left corner
+3. Click view All and click Sales from App Launcher
+4. Click on Leads tab
+5. Click on New button
+6. Select Salutation dropdown
+7. Enter the Last Name
+8. Enter the Company Name
+9. Click Save and Verify Leads name created */
+
 import {expect, test} from '@playwright/test'
 
 test('Create a Lead', async ({page}) => {
